1cb8b7114e2769b58e323fe54e6fd834dc367bc1
anekawa <hanseiten@gmail.com>
2019-06-19 19:03:20 +0900

    データ取得までデバッグまだ

--- backend/index.ts
+++ backend/index.ts
@@ -1,21 +1,78 @@
 import express from 'express'
 import { Request, Response } from 'express'
 import db from './db/connect'
+import axios, { AxiosResponse } from 'axios'
+import {
+  MatchReferenceDto,
+  MatchlistDto,
+  PartOfMatchDto,
+  PartOfParticipantIdentityDto,
+  PartOfParticipantDto,
+  PartOfPlayerDto,
+  Players,
+  Player
+} from './types'
 
 const app = express()
 
 // Serve the static files from the React app
 app.use(express.static('../build'))
 
-app.get('/past_games', (req: Request, res: Response) => {
-  const returnArray: string[] = []
-  db.any('select id from cyphers').then((ids: Array<{ id: string }>) => {
-    ids.map((set: { id: string }) => {
-      returnArray.push(set.id.toString())
+app.get('/past_games', async (req: Request, res: Response) => {
+  // 直近３ゲーム
+  const pastGameIds: number[] = []
+  const gamesResponse = await axios.get<MatchlistDto>(
+    'https://jp1.api.riotgames.com/lol/match/v4/matchlists/by-account/SmTe3BkIqi-OTAHezqR_gDtXt2dYLFMTLerAUDvQfaPpBzJLYWM530WP?endIndex=3&beginIndex=0&api_key=RGAPI-3962d085-0317-45ea-a989-4d40f5c8c14b'
+  )
+  gamesResponse.data.matches.map((game: MatchReferenceDto) => {
+    pastGameIds.push(game.gameId)
+  })
+
+  // プレイヤー情報
+  const players: Players[] = []
+  await pastGameIds.map(async pastGameId => {
+    const playersResponse = await axios.get<
+      PartOfMatchDto<
+        PartOfParticipantIdentityDto<PartOfPlayerDto>,
+        PartOfParticipantDto
+      >
+    >(
+      `https://jp1.api.riotgames.com/lol/match/v4/matches/${pastGameId}?api_key=RGAPI-3962d085-0317-45ea-a989-4d40f5c8c14b`
+    )
+    const blueTeamParticipantIds: number[] = []
+    const redTeamParticipantIds: number[] = []
+
+    await playersResponse.data.participants.map(
+      (participant: PartOfParticipantDto) => {
+        if (participant.teamId === 100) {
+          blueTeamParticipantIds.push(participant.participantId)
+        } else {
+          redTeamParticipantIds.push(participant.participantId)
+        }
+      }
+    )
+
+    const blueTeamParticipants: string[] = []
+    const redTeamParticipants: string[] = []
+    await playersResponse.data.participantIdentities.map(
+      participantIdentity => {
+        if (
+          blueTeamParticipantIds.includes(participantIdentity.participantId)
+        ) {
+          blueTeamParticipants.push(participantIdentity.player.summonerName)
+        } else {
+          redTeamParticipants.push(participantIdentity.player.summonerName)
+        }
+      }
+    )
+
+    await players.push({
+      blue: blueTeamParticipants,
+      red: redTeamParticipants
     })
   })
-  res.json({ arr: returnArray })
-})
 
+  res.json({ playerTeams: players })
+})
 const port = process.env.PORT || 5000
 app.listen(port)
---
+++ backend/types.ts
@@ -0,0 +1,49 @@
+export interface MatchlistDto {
+  matches: MatchReferenceDto[]
+  totalGames: number
+  startIndex: number
+  endIndex: number
+}
+
+export interface MatchReferenceDto {
+  lane: string
+  gameId: number
+  champion: number
+  platformId: string
+  season: number
+  queue: number
+  role: string
+  timestamp: number
+}
+
+export interface PartOfMatchDto<
+  PI extends PartOfParticipantIdentityDto<PartOfPlayerDto>,
+  P extends PartOfParticipantDto
+> {
+  participantIdentities: PI[]
+  participants: P[]
+}
+
+export interface PartOfParticipantIdentityDto<PL extends PartOfPlayerDto> {
+  player: PL
+  participantId: number
+}
+
+export interface PartOfParticipantDto {
+  participantId: number
+  teamId: 100 | 200
+}
+
+export interface PartOfPlayerDto {
+  summonerName: string
+}
+
+export interface Players {
+  blue: string[]
+  red: string[]
+
+}
+
+export interface Player {
+  name: string
+}
